3e29e4ace28b66412e784820300d641ae97d1ad8
Alex James <hello@alexjamesdesign.co.uk>
2021-02-01 22:54:42 +0000

    Trying to remove the loop on recipe and filter using only graphQL filter

--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -10,37 +10,33 @@ const recipePage = ({data: { recipe, tag }}) => {
     <>
 
     <SEO title="Home" />
-    {recipe.edges.map(({ node }, i) => {
-        return (
-            <section className="container flex space-x-4">
-                <section className="sidebar w-1/3">
+        <section className="container flex space-x-4">
+            <section className="sidebar w-1/3">
 
-                <div className="sidebar w-full bg-gray-200 p-6">
+            <div className="sidebar w-full bg-gray-200 p-6">
 
-                    <h1 className="w-full text-5xl period">Ingredients</h1>
-                    <p>{node.ingredients}</p>
+                <h1 className="w-full text-5xl period">Ingredients</h1>
+                <p>{recipe.ingredients}</p>
 
+            </div>
+
+            </section>
+
+            <section className="main-right w-2/3">
+            <div className="featured-section w-full bg-gray-200 p-6">
+                <h1 className="w-full text-5xl period">{recipe.name}</h1>
+                {tag.title}
+
+                <div className="recipe-stats bg-gray-300 p-10">
+                    <p>{recipe.preparationTime}</p>
+                    <p>{recipe.tag.title}</p>
                 </div>
 
-                </section>
-
-                <section className="main-right w-2/3">
-                <div className="featured-section w-full bg-gray-200 p-6">
-                    <h1 className="w-full text-5xl period">{node.name}</h1>
-                    {tag.title}
-
-                    <div className="recipe-stats bg-gray-300 p-10">
-                        <p>{node.preparationTime}</p>
-                        <p>{node.tag.title}</p>
-                    </div>
-
-                </div>
-
-                </section> 
+            </div>
 
             </section> 
-        )
-    })}
+
+        </section> 
     </>
   )
 }
@@ -49,25 +45,21 @@ export default recipePage
 
 export const query = graphql`
 query RecipePageQuery {
-  recipe: allDatoCmsRecipe(filter: {slug: {eq: "chicken-katsu-curry"}}) {
-    edges {
-      node { 
-        name
+  recipe: datoCmsRecipe(filter: {slug: {eq: "chicken-katsu-curry"}}) {
+    name
+    slug
+    recipePic {
+        url
+        alt
+        fluid(imgixParams: {auto: "compress", sharp: 10, h: "390", w: "740", fit: "fillmax", crop: "center" }) {
+        ...GatsbyDatoCmsFluid
+        }
+    }
+    preparationTime
+    ingredients
+    tag {
+        title
         slug
-        recipePic {
-          url
-          alt
-          fluid(imgixParams: {auto: "compress", sharp: 10, h: "390", w: "740", fit: "fillmax", crop: "center" }) {
-            ...GatsbyDatoCmsFluid
-          }
-        }
-        preparationTime
-        ingredients
-        tag {
-          title
-          slug
-        }
-      }
     }
   }
   tag: allDatoCmsTag {

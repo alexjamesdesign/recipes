fba812bc03c3948cd458e6a1cd8eda4e35f1e925
Alex James <hello@alexjamesdesign.co.uk>
2021-02-04 18:03:40 +0000

    Shuffle of dynamic page files to try and fix it

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,5 @@
-const path = require(`path`)
+const createRecipe = require(`./gatsby/createRecipe`)
 
-module.exports = async ({ actions, graphql }) => {
-  const { createPage } = actions
-
-  return new Promise((resolve, reject) => {
-    graphql(`
-    {
-        allDatoCmsRecipe {
-            edges {
-                node {
-                        slug
-                    }
-                }
-            }
-        }  
-    `).then(result => {
-      result.data.allDatoCmsRecipe.edges.map(edge => {
-        createPage({
-          path: `recipes/${edge.node.slug}`,
-          component: path.resolve(`./src/templates/recipe.js`),
-          context: {
-            slug: edge.node.slug,
-          },
-        })
-      })      
-      resolve()
-    })
-  })
+exports.createPages = async ({ actions, graphql }) => {
+  await createRecipe({ actions, graphql })
 }

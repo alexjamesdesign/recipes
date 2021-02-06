4278ddb98c4088fecdf06a13e08ead0af2a939a7
Alex James <hello@alexjamesdesign.co.uk>
2021-02-06 15:47:41 +0000

    Edits made to index to add hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,38 +3,31 @@ import Footer from "../components/footer"
 import SEO from "../components/seo"
 import Card from "../components/card"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
 
 
-const IndexPage = ({data: { page, tag }}) => {
+const IndexPage = ({data: { page, tag, hero }}) => {
   return (
     <>
 
       <SEO title="Home" />
-      <section className="container md:flex md:space-x-4">
-        <section className="sidebar md:w-1/3">
+      <section className="container md:flex flex-wrap md:space-y-4">
 
-          <div className="sidebar w-full bg-gray-200 p-6">
-
-            <h1 className="w-full text-5xl period">Tags</h1>
-
-            {tag.edges.map(({ node }, i) => {
-              return (
-                  <Link to="/blog" className="block" key={i}>{node.title}</Link>
-              )
-            })}
-
+        <section className="hero w-full bg-gray-200 relative">
+          <Img fluid={hero.image.fluid} className="absolute top-0" /> 
+          <div className="flex flex-col-reverse justify-center text-align-right h-full absolute top-0 w-full">
+            <p className="text-white font-anton text-5xl text-right pr-20 left-20 top-0">{hero.heroText}</p>
           </div>
-
         </section>
 
-        <section className="main-right md:w-2/3">
+        <section className="main-right w-full">
           <div className="featured-section w-full bg-gray-200 p-6">
             <h1 className="w-full text-5xl period">Featured</h1>
 
             <div className="card-container w-full flex flex-wrap">
               {page.edges.map(({ node }, i) => {
                 return (
-                  <div className="pr-4 pb-4 w-full sm:w-1/2" key={i}>
+                  <div className="pr-4 pb-4 w-full sm:w-1/3" key={i}>
                     <div className="w-full bg-white text-black p-2 border-2 border-gray-50" key={i} >
                       <Card slug={node.slug} name={node.name} image={node.recipePic} time={node.preparationTime} tags={node.tag.title} />
                     </div>
@@ -85,6 +78,16 @@ query IndexPageQuery {
         title 
       }
     }
+  }
+  hero: datoCmsHero {
+    image {
+      url
+      alt
+      fluid(imgixParams: {auto: "compress", sharp: 10, h: "390", w: "740", fit: "fillmax", crop: "center" }) {
+        ...GatsbyDatoCmsFluid
+      }
+    }
+    heroText 
   }
 }
 `
--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,58 +1,64 @@
 import React from "react"
 import Footer from "../components/footer"
 import SEO from "../components/seo"
+import Card from "../components/card"
+import { Link } from "gatsby"
 import Img from "gatsby-image"
-import { CgTime } from "@react-icons/all-files/cg/CgTime";
-import { CgTag } from "@react-icons/all-files/cg/CgTag";
 
 const recipesPage = ({data: { recipe, tag }}) => {
   return (
     <>
 
-    <SEO title="Home" />
-        <section className="container md:flex md:space-x-4">
+    <SEO title="Recipes" />
+      <section className="container md:flex md:space-x-4">
+        <section className="sidebar md:w-1/3">
 
-            <section className="sidebar md:w-1/3">
+          <div className="sidebar w-full bg-gray-200 p-6">
 
-                <div className="sidebar w-full bg-gray-200 p-6">
-
-                    <h1 className="w-full text-5xl period">Ingredients</h1>
-                    <div className="ingredients-wysiwyg" dangerouslySetInnerHTML={{ __html: recipe.ingredients }}></div>
-
-                </div>
-
-            </section>
-
-            <section className="main-right mt-6 md:mt-0 md:w-2/3">
-
-                <div className="featured-section w-full bg-gray-200 p-6">
-
-                    <h1 className="w-full text-5xl period">{recipe.name}</h1>
-                    
-                    <div className="recipe-stats bg-gray-300 p-4 my-4 flex gap-6">
-                        <p className="flex items-center"><CgTime className="mr-2" /> {`${recipe.preparationTime ? recipe.preparationTime+' mins' : "No time set"}`}</p> 
-                        <p className="flex items-center"><CgTag className="mr-2" /> {recipe.tag.title}</p> 
-                    </div>
-
-                    <div className="recipe-pics bg-gray-100">
+            <h1 className="w-full text-5xl period">Tags</h1>
+            <div className="flex space-x-4 radius-4 ">
+              {tag.edges.map(({ node }, i) => {
+                return (
+                  
+                    <Link to="/blog" className="h-15 w-1/2 relative flex justify-center align-middle" key={i}>
+                      <div className="h-15 text-white absolute z-10 pt-6 font-anton">
                         
-                        <Img fluid={recipe.recipePic.fluid} className="w-full h-30" alt={recipe.recipePic.alt} />
-
-                    </div>
-
-                    <div className="recipe-method bg-gray-200 py-2">
-                        
-                        <div className="method-wysiwyg" dangerouslySetInnerHTML={{__html: recipe.method }}></div>
-
-                    </div>
-
-                </div>
-
-            </section> 
+                          {node.title}
+                       
+                      </div>
+                      <Img fluid={node.image.fluid} className="w-full absolute h-full rounded-md" alt={node.image.alt} />
+                    </Link>
+                  
+                )
+              })}
+            </div>
+          </div>
 
         </section>
 
-        <Footer borderColor="border-white" backToTopButton />
+        <section className="main-right md:w-2/3">
+          <div className="featured-section w-full bg-gray-200 p-6">
+            <h1 className="w-full text-5xl period">Featured</h1>
+
+            <div className="card-container w-full flex flex-wrap">
+              {recipe.edges.map(({ node }, i) => {
+                return (
+                  <div className="pr-4 pb-4 w-full sm:w-1/2" key={i}>
+                    <div className="w-full bg-white text-black p-2 border-2 border-gray-50 rounded-md" key={i} >
+                      <Card slug={node.slug} name={node.name} image={node.recipePic} time={node.preparationTime} tags={node.tag.title} />
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+
+          </div>
+
+        </section> 
+
+      </section> 
+
+      <Footer borderColor="border-white" backToTopButton />
     </>
   )
 }
@@ -61,28 +67,44 @@ export default recipesPage
 
 export const query = graphql`
 query RecipePageQuery {
-  recipe: datoCmsRecipe(slug: {eq: "chicken-katsu-curry"}) {
-    name
-    slug
-    recipePic {
-        url
-        alt
-        fluid(imgixParams: {auto: "compress", sharp: 10, h: "260", w: "752", fit: "fillmax", crop: "center" }) {
-        ...GatsbyDatoCmsFluid
-        }
-    }
-    preparationTime
-    ingredients
-    method
-    tag {
-        title
+  recipe: allDatoCmsRecipe {
+    edges {
+      node { 
+        name
         slug
+        recipePic {
+          url
+          alt
+          fluid(imgixParams: {auto: "compress", sharp: 10, h: "390", w: "740", fit: "fillmax", crop: "center" }) {
+            ...GatsbyDatoCmsFluid
+          }
+        }
+        preparationTime
+        ingredients
+        tag {
+          title
+          slug
+          image {
+            alt
+            fluid(imgixParams: {auto: "compress", sharp: 10, h: "390", w: "740", fit: "fillmax", crop: "center" }) {
+              ...GatsbyDatoCmsFluid
+            }
+          }
+        }
+      }
     }
   }
   tag: allDatoCmsTag {
     edges {
       node { 
         title 
+        slug
+        image {
+          alt
+          fluid(imgixParams: {auto: "compress", sharp: 10, h: "390", w: "740", fit: "fillmax", crop: "center", bri: -15, blur: 35 }) {
+            ...GatsbyDatoCmsFluid
+          }
+        }
       }
     }
   }
